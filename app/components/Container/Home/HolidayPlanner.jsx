fe3cf554a1fbc3e61c36f5ea8ad86caf5c5694e4
Prakash-WebDad <prakash.s@webdads2u.com>
2026-02-18 12:19:16 +0530

    homepage aniamtion completed

--- app/components/Container/Home/HolidayPlanner.jsx
+++ app/components/Container/Home/HolidayPlanner.jsx
@@ -101,11 +101,11 @@ const HolidayPlanner = ({ activeSlugFromRoute }) => {
                 variants={item}
                 key={sub._id}
                 onClick={handleSlug(sub.slug)}
-                className={`px-4 py-2 rounded-full cursor-pointer whitespace-nowrap transition
+                className={`px-4 py-2 rounded-full cursor-pointer whitespace-nowrap transition  
                   ${
                     activeSlug === sub.slug
-                      ? "bg-red-100 text-red-600 font-medium"
-                      : "text-gray-700 hover:bg-red-100 hover:text-red-600"
+                      ? "bg-red-50 border-red-500 text-red-600 font-medium border"
+                      : "bg-white border-gray-300  hover:border-red-400 hover:text-red-600"
                   }
                 `}
               >
