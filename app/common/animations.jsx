416259d36799d5b655b11e216c343dd48c14b820
snegavenkat-esan <snega@webdads2u.com>
2026-02-19 16:57:07 +0530

    animation about

--- app/common/animations.jsx
+++ app/common/animations.jsx
@@ -81,6 +81,31 @@ export const heroFade = {
   },
 };
 
+export const fadeFromTop = {
+  hidden: { opacity: 0, y: -80 },   
+  visible: {
+    opacity: 1,
+    y: 0,                          
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fadeFromBottom = {
+  hidden: { opacity: 0, y: 80 },  
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
+
+
 export const staggerOnly = {
   hidden: {},
   visible: {
