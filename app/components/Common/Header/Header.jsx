b9aad7e5723e4cef8025e679465dd65a766ae734
Prakash-WebDad <prakash.s@webdads2u.com>
2026-02-20 17:28:18 +0530

    header issue fix

--- app/components/Common/Header/Header.jsx
+++ app/components/Common/Header/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
       </div>
 
       <header className="sticky top-0 z-9999 bg-white shadow-2xl">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center">
+        <div className="max-w-7xl mx-auto pl-4 sm:pl-6 lg:pl-8 h-16 flex items-center">
           <Link
             href="/"
             className="flex items-center gap-3 z-10 md:relative top-2"
