2013b206840fbc96551881b476f88b1394d1f5ae
Prakash-WebDad <prakash.s@webdads2u.com>
2026-02-21 14:54:04 +0530

    unnessery code remove

--- app/protectedRoute/ProtectedRoute.js
+++ app/protectedRoute/ProtectedRoute.js
@@ -1,31 +1,12 @@
 "use client";
 import { useSelectedLayoutSegments, useRouter } from "next/navigation";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import Header from "../components/Common/Header/Header";
 import Footer from "../components/Common/Footer/Footer";
-import "react-toastify/dist/ReactToastify.css";
-import { setupTokenRefresh, isLoginExpired } from "../utils/setupTokenRefresh";
 import WhatsAppButton from "../common/WhatsAppButton";
 
 export default function AppWrapper({ children }) {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  // const accessToken = useSelector((state) => state.auth.accessToken);
   const segments = useSelectedLayoutSegments();
   const isNotFound = segments.includes("not-found");
-
-  // useEffect(() => {
-  //   if (isLoginExpired()) {
-  //     localStorage.clear();
-  //     dispatch(logout());
-  //     router.push("/login");
-  //     return;
-  //   }
-  //   if (accessToken) {
-  //     setupTokenRefresh(dispatch, logout, refreshToken);
-  //   }
-  // }, [accessToken]);
 
   return (
     <>
